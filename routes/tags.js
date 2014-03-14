f8dce708fe98285e084e9d84dca4c45ce17decab
vagrant <vagrant@precise32.(none)>
2014-03-14 00:53:53 +0000

    tags fixed with ajax

--- routes/tags.js
+++ routes/tags.js
@@ -17,14 +17,18 @@ exports.search = function(req, res){
 			altItemModel.AltItem.find({"tags": tag}).exec(callbackalt);
 			function callbackalt(err, altItems){
 				for(var i = 0; i<altItems.length; i++){
+					altItems[i] = altItems[i].toObject();
+					altItems[i].name = altItems[i].name.toUpperCase();
+					altItems[i].brand = altItems[i].brand.toUpperCase();
 					if(myalikes.indexOf(altItems[i]._id) != -1){
-						altItems[i] = altItems[i].toObject();
 						altItems[i].liked = '1';
 					}
 				}
 				for(var i = 0; i<designerItems.length; i++){
+					designerItems[i] = designerItems[i].toObject();
+					designerItems[i].name = designerItems[i].name.toUpperCase();
+					designerItems[i].brand = designerItems[i].brand.toUpperCase();
 					if(mydlikes.indexOf(designerItems[i]._id) != -1){
-						designerItems[i] = designerItems[i].toObject();
 						designerItems[i].liked = '1';
 					}
 				}
@@ -33,7 +37,7 @@ exports.search = function(req, res){
 						"name": req.session.user,
 						"imageURL": req.session.imageURL
 					},
-					"tag": tag,
+					"tag": tag.toUpperCase(),
 					"designerItems": designerItems,
 					"altItems": altItems
 				};
